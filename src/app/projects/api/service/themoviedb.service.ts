f2b4675b2c816087dbef9924c8b40c0be4d87b21
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-05-16 20:38:55 +0200

    figuring out how tabs work; some services created.

--- src/app/projects/api/service/themoviedb.service.ts
+++ src/app/projects/api/service/themoviedb.service.ts
@@ -3,17 +3,49 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 const apiKey = "e53bbde3abe182705b021e68f89d3006"
+
+const movieOrTvShow = "movie"
+
+const id = ""
+
+const genres = ""
+
+const page = "" 
+
+const id = ""
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class ThemoviedbService {
 
   constructor(private http: HttpClient) { 
 
   }
 
-  getLatestMovies(type: string): Observable<any> {
-    const callURL = `https://api.themoviedb.org/3/movie/latest?api_key=${apiKey}&language=en-US`;
+  getLatestMovies(movieOrTvShow: string): Observable<any> {
+    const callURL = `https://api.themoviedb.org/3/${movieOrTvShow}/latest?api_key=${apiKey}&language=en-US`;
+    return this.http.get(callURL);
+  }
+
+  getTopRatedMovies(movieOrTvShow: string, page: number): Observable<any> {
+    const callURL = `https://api.themoviedb.org/3/${movieOrTvShow}/top_rated?api_key=${apiKey}&language=en-US&page=${page}`;
+    return this.http.get(callURL);
+  }
+
+  getPopularMovies(movieOrTvShow: string, page: number, genres: string): Observable<any> {
+    const callURL = `https://api.themoviedb.org/3/${movieOrTvShow}/popular?api_key=${apiKey}&language=en-US&page=${page}&with_genres=${genres}`;
+    return this.http.get(callURL);
+  }
+
+  getGenreLists(movieOrTvShow: string): Observable<any> {
+    const callURL = `https://api.themoviedb.org/3/genre/${movieOrTvShow}/list?api_key=${apiKey}&language=en-US`;
+    return this.http.get(callURL);
+  }
+
+  getDetails(movieOrTvShow: string, id: string): Observable<any> {
+    const callURL = `https://api.themoviedb.org/3/${movieOrTvShow}/${id}?api_key=${apiKey}&language=en-US`;
     return this.http.get(callURL);
   }
 }
