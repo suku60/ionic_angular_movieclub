c0522f0d74245c68f163e682197f04a784bf813f
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-05-17 13:48:47 +0200

    success adding max number of movies.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,7 @@ import { TheMovieDBService } from '../projects/api/service/themoviedb.service';
 })
 export class Tab1Page implements OnInit {
   modelMovieOrTvShow = 'movie';
+  max_movies = [];
   slider = [];
 
   constructor ( private service:  TheMovieDBService) {}
@@ -19,15 +20,27 @@ export class Tab1Page implements OnInit {
 
   SliderContainerInit() { 
     this.service.getNowPlayingMovies(this.modelMovieOrTvShow).subscribe(LatestMoviesObj => {
-      console.log("response", LatestMoviesObj?.results, "maping?", LatestMoviesObj?.results?.forEach(latestMoviesResult => {
-        console.log(latestMoviesResult.id)
-      }))
+      // console.log("response", LatestMoviesObj?.results, "maping?", LatestMoviesObj?.results?.forEach(latestMoviesResult => {
+      //   console.log(latestMoviesResult.id)
+      // }))
 
-      LatestMoviesObj?.results?.forEach(latestMovies => {
+      this.max_movies = Object.entries(LatestMoviesObj?.results).slice(0,11);
+
+      console.log("max?;", this.max_movies)
+
+      this.max_movies.forEach(latestMovies => {
+        console.log("console log inside mapping", latestMovies[1])
         this.slider.push({
-          id: latestMovies.id
+          modelItem: latestMovies,
+          id: latestMovies.id,
+          title: latestMovies.title,
+          image: latestMovies.backgrop_path,
+          poster: latestMovies.poster_path,
+          overview: latestMovies.overview,
+          genres: latestMovies.genre_ids,
+          rating: latestMovies.vote_average
         },
-        console.log("working?")
+        console.log("working?", this.slider)
         )
         
       }
