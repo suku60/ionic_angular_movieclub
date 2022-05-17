997e2c9fbdc4741c43ced9ffb4beebfba098b1de
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-05-17 13:30:08 +0200

    Mapping succesfull. Designing slider and it's content.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,8 +6,9 @@ import { TheMovieDBService } from '../projects/api/service/themoviedb.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
   modelMovieOrTvShow = 'movie';
+  slider = [];
 
   constructor ( private service:  TheMovieDBService) {}
   
@@ -17,12 +18,25 @@ export class Tab1Page implements OnInit{
   }
 
   SliderContainerInit() { 
-    this.service.getNowPlayingMovies(this.modelMovieOrTvShow).subscribe(LatestMovies =>
-      
-      
-      console.log("response", LatestMovies?.results, "maping?", LatestMovies?.results?.forEach(latestMoviesResult => {
+    this.service.getNowPlayingMovies(this.modelMovieOrTvShow).subscribe(LatestMoviesObj => {
+      console.log("response", LatestMoviesObj?.results, "maping?", LatestMoviesObj?.results?.forEach(latestMoviesResult => {
         console.log(latestMoviesResult.id)
       }))
+
+      LatestMoviesObj?.results?.forEach(latestMovies => {
+        this.slider.push({
+          id: latestMovies.id
+        },
+        console.log("working?")
+        )
+        
+      }
+      
+      )
+    }
+      
+      
+      
       )
 
   }
